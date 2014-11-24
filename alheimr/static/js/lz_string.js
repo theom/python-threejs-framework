c649a35f4e72521305a1658c08ed1d065969dc4b
theom <jens@axonehf.com>
2014-11-24 21:21:27 +0000

    Change the license from MIT to Apache License 2.0

--- alheimr/static/js/lz_string.js
+++ alheimr/static/js/lz_string.js
@@ -1,6 +1,18 @@
 
-// Copyright (c) 2013 Pieroxy <pieroxy@pieroxy.net>
-// Copyright 2014 Jens Páll Hafsteinsson, https://www.axonehf.com
+// Copyright 2013 Pieroxy <pieroxy@pieroxy.net>
+// Copyright 2014 Jens Páll Hafsteinsson
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 // @constructor
 alheimr.lz_string = function lz_string()
